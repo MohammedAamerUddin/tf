266acd3b144ec163c324f196228a0439d9676fe4
Mohammed Aameruddin <mdaameruddin01@gmail.com>
2026-03-27 21:18:49 +0530

    .

---
+++ aws/aws-ec2/main.tf
@@ -0,0 +1,29 @@
+# variable "region" {
+#   description = "This the default value of region"
+#   type = string
+#   default = "us-east-1"
+# }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.38.0"
+    }
+  }
+}
+
+provider "aws" {
+  # region = "us-east-1"
+  region = var.region
+}
+
+resource "aws_instance" "instance" {
+  ami           = "ami-02dfbd4ff395f2a1b"
+  instance_type = "t3.micro"
+
+  #   Optional
+  tags = {
+    Name = "terrform_ec2"
+  }
+}
